78d51e587242d6da1487909ce37c664166fb903d
discreted66 <953831480@qq.com>
2025-01-14 16:32:51 -0800

    用例补全

---
+++ packages/vue/src/date-panel/__tests__/date-panel.test.ts
@@ -0,0 +1,17 @@
+import { mountPcMode } from '@opentiny-internal/vue-test-utils'
+import { describe, expect, test } from 'vitest'
+import { DatePanel } from '@opentiny/vue-date-panel'
+
+describe('PC Mode', () => {
+  const mount = mountPcMode
+
+  test('static', () => {
+    const wrapper = mount(DatePanel, {
+      props: {
+        modelValue: '2025-01-14'
+      }
+    })
+    expect(wrapper.classes()).toContain('tiny-picker-panel')
+    expect(wrapper.find('td .current').exists()).toBe(true)
+  })
+})
